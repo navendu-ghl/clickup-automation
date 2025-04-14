b52e844a4a588bddaf98a38ea9b5b7e4b05aa45a
Navendu Duari <navendu.duari@gmail.com>
2025-04-15 00:24:34 +0530

    fix: change request type from get to all

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/run', handleRun);
+app.all('/run', handleRun);
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
